3b7d865360ddc103721d98125cb72b2b81f2a72f
Connor Proctor <connorproctor@users.noreply.github.com>
2018-01-17 22:38:57 -0800

    Fix bug with suggest_random_team_service

--- app/services/suggest_random_teams_service.rb
+++ app/services/suggest_random_teams_service.rb
@@ -6,7 +6,7 @@ class SuggestRandomTeamsService
     game_in_progress = Game.in_progress.includes(:results).first
     raise NoGameInProgressError if game_in_progress.nil?
 
-    players_shuffled = game.players.map(&:slack_user_name).shuffle
+    players_shuffled = game_in_progress.players.map(&:slack_user_name).shuffle
     "Randomized matchup: #{players_shuffled[0]} and #{players_shuffled[1]} vs. #{players_shuffled[2]} and #{players_shuffled[3]}"
   end
 end
