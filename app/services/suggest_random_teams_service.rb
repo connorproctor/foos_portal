4cd57c446aef510f7023db954888579798b63dd9
rynechaloux <rynechaloux@gmail.com>
2018-08-29 14:43:13 -0700

    don't include rank since at least one person isnt

--- app/services/suggest_random_teams_service.rb
+++ app/services/suggest_random_teams_service.rb
@@ -7,6 +7,6 @@ class SuggestRandomTeamsService
     raise NoGameInProgressError if game_in_progress.nil?
 
     players_shuffled = game_in_progress.players.map(&:slack_user_name).shuffle
-    "Randomized matchup: #{players_shuffled[0].mention_and_rank} and #{players_shuffled[1].mention_and_rank} vs. #{players_shuffled[2].mention_and_rank} and #{players_shuffled[3].mention_and_rank}"
+    "Randomized matchup: #{players_shuffled[0].mention} and #{players_shuffled[1].mention} vs. #{players_shuffled[2].mention} and #{players_shuffled[3].mention}"
   end
 end
