4c58ee80ad55a9320f9feb2b3d44970a62c72e31
rynechaloux <rynechaloux@gmail.com>
2018-08-29 10:01:58 -0700

    Add @ to randomized matchups to tag players

--- app/services/suggest_random_teams_service.rb
+++ app/services/suggest_random_teams_service.rb
@@ -7,6 +7,6 @@ class SuggestRandomTeamsService
     raise NoGameInProgressError if game_in_progress.nil?
 
     players_shuffled = game_in_progress.players.map(&:slack_user_name).shuffle
-    "Randomized matchup: #{players_shuffled[0]} and #{players_shuffled[1]} vs. #{players_shuffled[2]} and #{players_shuffled[3]}"
+    "Randomized matchup: @#{players_shuffled[0]} and @#{players_shuffled[1]} vs. @#{players_shuffled[2]} and @#{players_shuffled[3]}"
   end
 end
