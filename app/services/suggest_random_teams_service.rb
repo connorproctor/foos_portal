16cdcc5817e8dacee13e0eec4cfa737e20ba356a
Connor Proctor <connorproctor@gmail.com>
2018-08-29 18:54:15 -0700

    Mention players instead of slack_user_name strings

--- app/services/suggest_random_teams_service.rb
+++ app/services/suggest_random_teams_service.rb
@@ -6,7 +6,7 @@ class SuggestRandomTeamsService
     game_in_progress = Game.in_progress.includes(:results).first
     raise NoGameInProgressError if game_in_progress.nil?
 
-    players_shuffled = game_in_progress.players.map(&:slack_user_name).shuffle
-    "Randomized matchup: #{players_shuffled[0].mention} and #{players_shuffled[1].mention} vs. #{players_shuffled[2].mention} and #{players_shuffled[3].mention}"
+    players_shuffled = game_in_progress.players.map(&:mention).shuffle
+    "Randomized matchup: #{players_shuffled[0]} and #{players_shuffled[1]} vs. #{players_shuffled[2]} and #{players_shuffled[3]}"
   end
 end
